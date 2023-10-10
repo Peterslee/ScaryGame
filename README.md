0c4d4d646a24d529d1d1d83061c38722e5c26eea
Peterslee <sastrafrayogi@gmail.com>
2023-10-11 00:13:43 +0800

    ngeri

---
+++ README.md
@@ -0,0 +1,14 @@
+# Scary Maze Game
+
+A browser game.
+
+### https://hqscarymaze.onrender.com
+
+## Scary Maze
+
+![Scary Maze](./Scary_Maze.png?raw=true "Scary Maze")
+
+## Version History
+
+- 0.1
+  - Initial Release
---
+++ app.js
@@ -0,0 +1,43 @@
+const levelButton = document.querySelector(".level-button");
+
+const levelOne = document.querySelector(".level-one");
+const levelTwo = document.querySelector(".level-two");
+
+const mazeLevel = document.querySelector(".maze-level");
+const mazeMessage = document.querySelector(".maze-message");
+const mazeMessage2 = document.querySelector(".maze-message2");
+
+const endPicture = document.querySelector(".end-picture");
+const endSound = document.querySelector(".end-sound");
+
+const collisionCheck = (value) => {
+  if (value === "maze-border") {
+    alert("You lost! Try again!");
+  }
+  if (value === "finish") {
+    levelButton.style.opacity = 1;
+    levelButton.style.pointerEvents = "all";
+    levelOne.style.pointerEvents = "none";
+  }
+  if (value === "end-game") {
+    endSound.play();
+    endPicture.style.display = "block";
+    document.body.style.background = "black";
+  }
+};
+
+window.addEventListener("mousemove", (e) => {
+  console.log(e.target.classList.value);
+  let check = e.target.classList.value;
+  collisionCheck(check);
+});
+
+levelButton.addEventListener("click", () => {
+  levelOne.style.display = "none";
+  levelTwo.style.display = "block";
+  levelButton.style.opacity = 0;
+  levelButton.style.pointerEvents = "none";
+  mazeLevel.textContent = "Level 2";
+  mazeMessage.textContent = "Mantap! Satu langkah lagi!";
+  mazeMessage2.textContent = "Your my dream! nur";
+});
